4e53f945801455970330b0251e27ed7b83ea81f4
Vipul Asri <vipulasri.2007@gmail.com>
2021-09-26 17:22:57 +0530

    update: instagram story active border gradient

--- app/src/main/java/com/vipulasri/jetinstagram/ui/components/Gradient.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/components/Gradient.kt
@@ -1,64 +1,75 @@
 package com.vipulasri.jetinstagram.ui.components
+
 import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.draw.drawWithContent
-import androidx.compose.ui.graphics.BlendMode
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shape
-import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.*
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 
 fun Modifier.diagonalGradientTint(
-    colors: List<Color>,
-    blendMode: BlendMode
+  colors: List<Color>,
+  blendMode: BlendMode
 ) = drawWithContent {
-    drawContent()
-    drawRect(
-        brush = Brush.linearGradient(colors),
-        blendMode = blendMode
-    )
+  drawContent()
+  drawRect(
+    brush = Brush.linearGradient(colors),
+    blendMode = blendMode
+  )
 }
 
 fun Modifier.offsetGradientBackground(
-    colors: List<Color>,
-    width: Float,
-    offset: Float = 0f
+  colors: List<Color>,
+  width: Float,
+  offset: Float = 0f
 ) = background(
-    Brush.horizontalGradient(
-        colors,
-        startX = -offset,
-        endX = width - offset,
-        tileMode = TileMode.Mirror
-    )
+  Brush.horizontalGradient(
+    colors,
+    startX = -offset,
+    endX = width - offset,
+    tileMode = TileMode.Mirror
+  )
 )
 
 fun Modifier.diagonalGradientBorder(
-    colors: List<Color>,
-    borderSize: Dp = 2.dp,
-    shape: Shape
-) = border(
+  colors: List<Color>,
+  borderSize: Dp = 2.dp,
+  shape: Shape,
+  isFromRight: Boolean = false
+) = composed {
+
+  val (start, end) = if (isFromRight) {
+    Pair(
+      Offset(100f, 0.0f),
+      Offset(0.0f, 100.0f)
+    )
+  } else {
+    Pair(Offset.Zero, Offset.Infinite)
+  }
+
+  border(
     width = borderSize,
-    brush = Brush.linearGradient(colors),
+    brush = Brush.linearGradient(colors = colors, start = start, end = end),
     shape = shape
-)
+  )
+}
 
 fun Modifier.fadeInDiagonalGradientBorder(
-    showBorder: Boolean,
-    colors: List<Color>,
-    borderSize: Dp = 2.dp,
-    shape: Shape
+  showBorder: Boolean,
+  colors: List<Color>,
+  borderSize: Dp = 2.dp,
+  shape: Shape
 ) = composed {
-    val animatedColors = List(colors.size) { i ->
-        animateColorAsState(if (showBorder) colors[i] else colors[i].copy(alpha = 0f)).value
-    }
-    diagonalGradientBorder(
-        colors = animatedColors,
-        borderSize = borderSize,
-        shape = shape
-    )
+  val animatedColors = List(colors.size) { i ->
+    animateColorAsState(if (showBorder) colors[i] else colors[i].copy(alpha = 0f)).value
+  }
+  diagonalGradientBorder(
+    colors = animatedColors,
+    borderSize = borderSize,
+    shape = shape
+  )
 }
--- app/src/main/java/com/vipulasri/jetinstagram/ui/home/StoryUi.kt
+++ app/src/main/java/com/vipulasri/jetinstagram/ui/home/StoryUi.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberImagePainter
 import com.vipulasri.jetinstagram.ui.components.diagonalGradientBorder
@@ -20,22 +19,23 @@ import com.vipulasri.jetinstagram.ui.components.diagonalGradientBorder
 fun StoryImage(imageUrl: String) {
   val shape = CircleShape
   Box(
-      modifier = Modifier
-          .diagonalGradientBorder(
-              colors = listOf(
-                  Color(0xFFd6249f),
-                  Color(0xFFfd5949),
-                  Color(0xFFfdf497),
-              ),
-              shape = shape
-//              ,isFromRight = true
-          )
+    modifier = Modifier
+      .diagonalGradientBorder(
+        colors = listOf(
+          Color(0xFFd71069),
+          Color(0xFFe25d6a),
+          Color(0xFFe9ad55),
+        ),
+        shape = shape,
+        isFromRight = true
+      )
   ) {
     Box(
-        modifier = Modifier.size(60.dp)
-            .padding(3.dp)
-            .background(color = Color.LightGray, shape = shape)
-            .clip(shape)
+      modifier = Modifier
+        .size(60.dp)
+        .padding(3.dp)
+        .background(color = Color.LightGray, shape = shape)
+        .clip(shape)
     ) {
       Image(
         painter = rememberImagePainter(imageUrl),
